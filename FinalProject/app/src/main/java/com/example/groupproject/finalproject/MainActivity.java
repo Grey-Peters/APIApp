c02b6b8d5eb26cd67bc63c4040669229d8ffeb3b
Grey-Peters <seattlenewsguy@gmail.com>
2015-06-07 15:35:21 -0700

    Initial Test Changes
    Test Changes 6

--- FinalProject/app/src/main/java/com/example/groupproject/finalproject/MainActivity.java
+++ FinalProject/app/src/main/java/com/example/groupproject/finalproject/MainActivity.java
@@ -68,7 +68,7 @@ public class MainActivity extends ActionBarActivity {
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,Bundle savedInstanceState) {
-        View view = inflater.inflate(R.layout.splash, container, false);
+        View view = inflater.inflate(R.layout.activity_main, container, false);
 
         loginButton = (LoginButton) view.findViewById(R.id.login_button);
         loginButton.setReadPermissions("user_friends");
